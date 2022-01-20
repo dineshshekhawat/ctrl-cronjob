eaffd99a94eb730a31780545561ccfd1ec54874b
Dinesh <dinesh@geotrackers.com>
2022-01-20 22:32:12 +0530

    first commit

---
+++ ctrl-cronjob-start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# To run as background process use following command
+# nohup ./ctrl-cronjob.sh >/dev/null 2>&1 &
+
+# To kill the background process use following command
+# ps ax | grep ctrl-cronjob-start.sh
+# Ignore the grep --color=auto process and check Process ID of the other process
+# Do kill -9 <process-id>
+
+DIRECTORY_PATH="$HOME/ctrl-cronjob"
+FILE_PATH="$DIRECTORY_PATH/ctrl-cronjob.log"
+MESSAGE_PREFIX="Command execution done at:"
+SLEEP_SECONDS=10
+
+#------------------------------------------#
+# Create a directory if it does not exist. #
+#------------------------------------------#
+# Note the "-p" option in the mkdir        #
+# command which creates directories        #
+# recursively.                             #
+#------------------------------------------#
+createDirectory() {
+	mkdir -p -- "$1"
+}
+
+executeCommand() {
+	echo "$MESSAGE_PREFIX $(date +"%d-%m-%Y %T.%N %Z")" > $FILE_PATH
+	xdotool key ctrl
+}
+
+runScript() {
+	createDirectory $DIRECTORY_PATH
+	executeCommand
+}
+
+runLoop() {
+	while true
+	do
+	 runScript
+	 sleep $SLEEP_SECONDS
+	done
+}
+
+runLoop
